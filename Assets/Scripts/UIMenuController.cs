27dec983aa2c34c51b9ddb2aab44bd85d56a98ce
darioft <dariofernandeztorre@gmail.com>
2022-09-30 13:35:35 -0300

    Title screen
    - added text title
    - added entry box for name
    - added start button with name validation

---
+++ Assets/Scripts/UIMenuController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIMenuController : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField nameInput;
+    [SerializeField] private Button startButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CheckStatus()
+    {
+        if (nameInput.text != "")
+        {
+            startButton.interactable = true;
+        }
+        else startButton.interactable = false;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
